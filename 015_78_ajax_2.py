2073adfc251cc32252d690f127dce8fd81d747b9
guofi <guofi@yahoo.com>
2024-03-22 14:53:11 +0800

    bug: ajax demo2
    * 头条街拍图片
    * api接口变了，不能获取

---
+++ 015_78_ajax_2.py
@@ -0,0 +1,80 @@
+from urllib.parse import urlencode
+import requests
+import os
+from hashlib import md5
+from multiprocessing.pool import Pool
+
+'''
+
+头条搜索 （街拍）
+
+'''
+
+headers = {
+    'Host': 'm.weibo.cn',
+    'Referer': 'https://m.weibo.cn/u/2830678474',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+
+def get_page(offset):
+    params = {
+        'offset': offset,
+        'format': 'json',
+        'keyword': ' 街拍 ',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '3',
+    }
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(params)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError:
+        return None
+
+
+def get_images(json):
+    if json.get('data'):
+        for item in json.get('data'):
+            title = item.get('title')
+            images = item.get('image_detail')
+            for image in images:
+                yield {'image': image.get('url'),
+                       'title': title
+                       }
+
+
+def save_image(item):
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+    try:
+        response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print('Already Downloaded', file_path)
+    except requests.ConnectionError:
+        print('Failed to Save Image')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+GROUP_START = 1
+GROUP_END = 20
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
